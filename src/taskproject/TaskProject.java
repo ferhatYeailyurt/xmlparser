4d0849e391cfc03808c060f002558385a3565a30
furkan <mfg263@gmail.com>
2017-08-24 17:28:02 +0300

    okuma with future

--- src/taskproject/TaskProject.java
+++ src/taskproject/TaskProject.java
@@ -11,11 +11,12 @@ package taskproject;
  */
 
 import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Random;
 import static javax.management.Query.attr;
- 
+
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.transform.Transformer;
@@ -24,19 +25,24 @@ import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
- 
+
+import SayiOlustur.Sayi;
 import org.w3c.dom.Attr;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
+import org.xml.sax.SAXException;
 import xmlcontroller.XmlOkuYaz;
 import SayiOlustur.RandomSayiOlusturClass;
 import SayiOlustur.Sayilar;
 import SayiOlustur.SelectionSort;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 import javax.xml.parsers.ParserConfigurationException;
 public class TaskProject {
 
@@ -45,11 +51,8 @@ public class TaskProject {
      */
     public static void main(String[] args)  {
         
-       yazmaIslemi();
-        
-        
-        
-        
+       //yazmaIslemi();
+
   }
     
     public static void yazmaIslemi()
@@ -87,8 +90,40 @@ public class TaskProject {
        
     }
     
-    public static void okumaIslemi()
+    public static List<Sayilar> okumaIslemi()
     {
-        
+        String prefix="cikti";
+
+        List<CompletableFuture<Sayilar>> cfs=new ArrayList<>();
+        for(int i=0; i<10; i++){
+            final int dosyaNumarasi = i+1;
+            cfs.add(CompletableFuture.supplyAsync(() -> {
+                try {
+                    return XmlOkuYaz.xmlOku((prefix + (dosyaNumarasi)));
+                } catch (ParserConfigurationException e) {
+                    e.printStackTrace();
+                } catch (SAXException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }));
+        }
+
+        CompletableFuture.allOf(cfs.toArray(new CompletableFuture[cfs.size()])).join();
+        List<Sayilar> sayilar = cfs.stream().map(f -> {
+            try {
+                return f.get();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }).collect(Collectors.toList());
+
+
+        return sayilar;
     }
 }
